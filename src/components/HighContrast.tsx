6b911d9b721ec4d69470b82562a0f9f6673f9479
Julia <julia.konzett@digit-one.com>
2025-04-24 13:50:57 +0200

    added text for frontpage

--- src/components/HighContrast.tsx
+++ src/components/HighContrast.tsx
@@ -2,6 +2,18 @@ export default function HighContrast() {
   return (
     <div>
       <h2 className='font-medium text-4xl my-8'># Hochkontrast</h2>
+      <p>
+        Für den Hochkontrastmodus wurde ein interaktiver Funktionsbutton
+        implementiert, der es Nutzenden ermöglicht, per Klick zwischen dem
+        Standard- und dem Hochkontrastmodus zu wechseln. Der Button ist als
+        semantisches HTML-Element "button" umgesetzt und wurde mit dem Attribut
+        aria-label="Kontrastmodus ein- oder ausschalten" versehen, um assistiven
+        Technologien (wie Screenreadern) eine eindeutige Beschreibung seiner
+        Funktion bereitzustellen. Zusätzlich wurde der Button mit der CSS-Klasse
+        custom-focus ausgestattet, die durch visuelle Hervorhebung (z. B. rote
+        Umrandung bei Tastaturfokus) sicherstellt, dass er auch für Nutzende mit
+        eingeschränkter Mausbedienung zugänglich ist.
+      </p>
     </div>
   );
 }
--- src/components/ReducedNavigation.tsx
+++ src/components/ReducedNavigation.tsx
@@ -2,6 +2,20 @@ export default function ReducedNavigation() {
   return (
     <div>
       <h2 className='font-medium text-4xl my-8'># Reduzierte Navigation</h2>
+      <p>
+        Die Navigation ist in ein "nav"-Element eingebettet, wodurch
+        Screenreader und andere assistive Technologien die Navigation als
+        eigenständigen Bereich erkennen können. Jeder Navigationspunkt ist als
+        klickbarer "a"-Link realisiert und enthält ein präzises aria-label, das
+        die Funktion des Links eindeutig beschreibt. Dadurch wird die Navigation
+        auch für Nutzende mit Screenreadern oder anderen Hilfsmitteln
+        vollständig zugänglich. Zudem wurde auf eine klare Tastaturbedienbarkeit
+        geachtet: Alle Links sind über die Tab-Taste erreichbar und mit der
+        Utility-Klasse custom-focus versehen, die eine deutliche visuelle
+        Fokusanzeige bei Tastatursteuerung ermöglicht. Damit stellt die
+        Navigation sowohl in visueller als auch in technischer Hinsicht ein
+        barrierefreies zentrales Steuerelement der Anwendung dar.
+      </p>
     </div>
   );
 }
